03d0ad77b92a69ced8bb1717da52059b091695fc
dgehusji <gs1rs.korea@hotmail.com>
2024-05-05 20:03:23 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-function isValidSudoku(board) {
-  const rows = new Array(9).fill().map(() => new Array(9).fill(0));
-  const cols = new Array(9).fill().map(() => new Array(9).fill(0));
-  const boxes = new Array(9).fill().map(() => new Array(9).fill(0));
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      if (board[i][j] !== ".") {
-        const num = Number(board[i][j]) - 1;
-        const k = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-        if (rows[i][num] || cols[j][num] || boxes[k][num]) return false;
-        rows[i][num] = 1;
-        cols[j][num] = 1;
-        boxes[k][num] = 1;
-      }
+function moveZeroes(nums) {
+  let index = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[index++] = nums[i];
     }
   }
-  return true;
+  for (let i = index; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
 }
